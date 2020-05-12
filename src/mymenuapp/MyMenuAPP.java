b66d4445cd73999373bf29eaf975772e03ecf372
rodrigo govea <rodry.gm.isc@live.com.mx>
2020-05-12 15:46:28 -0500

    se agregaron dos líneas nuevas

---
+++ src/mymenuapp/MyMenuAPP.java
@@ -0,0 +1,10 @@
+package mymenuapp;
+public class MyMenuAPP 
+{
+    public static void main(String[] args) 
+    {
+        myWindow laVentana = new myWindow();
+        laVentana.setVisible(true);
+        
+    }   
+}
